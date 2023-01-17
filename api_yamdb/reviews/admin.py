a387811cc30639010f96f467690a5cb203dab6ef
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-17 21:50:43 +0400

    Added Genre and Category models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Category, Genre
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+    )
+    search_fields = ("name",)
+    list_filter = ("id",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+    )
+    search_fields = ("name",)
+    list_filter = ("id",)
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Category, CategoryAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Common(models.Model):
+    """Общая модель для Category и Genre"""
+
+    name = models.TextField(verbose_name="Наименование", max_length=100)
+    slug = models.SlugField("slug", unique=True, db_index=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(Common):
+    """Модель категорий."""
+
+    class Meta(Common.Meta):
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Genre(Common):
+    """Модель жанров."""
+
+    class Meta(Common.Meta):
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
