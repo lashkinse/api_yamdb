775ab1c86634d1bc64d0d5cc858a51929cb49f16
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-17 19:44:04 +0400

    Added custom User model

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -2,15 +2,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -55,16 +51,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "api_yamdb.wsgi.application"
 
-# Database
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -81,8 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -93,8 +83,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = ((BASE_DIR / "static/"),)
+
+AUTH_USER_MODEL = "users.User"
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = "admin"
+    MODERATOR = "moderator"
+    USER = "user"
+    USER_ROLE_CHOICES = (
+        (ADMIN, "admin"),
+        (MODERATOR, "moderator"),
+        (USER, "user"),
+    )
+
+    username = models.CharField(
+        unique=True,
+        max_length=30,
+        verbose_name="Имя пользователя",
+    )
+    email = models.EmailField(
+        unique=True,
+        max_length=100,
+        verbose_name="Адрес электронной почты",
+    )
+    role = models.CharField(
+        max_length=10,
+        choices=USER_ROLE_CHOICES,
+        default=USER,
+        verbose_name="Роль пользователя",
+    )
+    bio = models.TextField(
+        blank=True,
+        verbose_name="Биография пользователя",
+    )
+
+    @property
+    def is_admin(self):
+        return self.role == User.ADMIN or self.is_staff or self.is_superuser
+
+    @property
+    def is_moderator(self):
+        return self.role == User.MODERATOR
