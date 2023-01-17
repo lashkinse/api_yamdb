39605b2bf6e9736552689e3b6ba268676fbefe22
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-18 00:32:34 +0400

    added UserAdmin in admin panel

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+    )
+    search_fields = ("username",)
+    list_filter = ("role",)
+
+
+admin.site.register(User, UserAdmin)
