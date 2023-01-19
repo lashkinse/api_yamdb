71d5e26bf29b14000a80b95ec86e98528621972f
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-20 00:57:18 +0400

    Добавил django-filter

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from api.permissions import IsAdminOrReadOnly
 from api.serializers import CategorySerializer, GenreSerializer
 from reviews.models import Category, Genre
 
@@ -7,8 +8,10 @@ from reviews.models import Category, Genre
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrReadOnly,)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_filters",
     "rest_framework",
     "api.apps.ApiConfig",
     "users.apps.UsersConfig",
