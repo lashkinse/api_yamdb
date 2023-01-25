20f606b1705e32c9fbc2d53fe6c9264bcddd6080
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-25 22:30:37 +0400

    Добавил регистрацию пользователя, генерицию и отправку кодов подтверждения

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from reviews.models import Category, Genre, Title, Review, Comment
+from users import validators
 
 User = get_user_model()
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор категорий"""
+
     class Meta:
         model = Category
         fields = (
@@ -16,6 +20,8 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанров"""
+
     class Meta:
         model = Genre
         fields = (
@@ -25,6 +31,8 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор произведений"""
+
     genre = GenreSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
 
@@ -40,12 +48,16 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор комментариев"""
+
     class Meta:
         model = Comment
         fields = ("id", "text", "author", "pub_date")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор пользователей"""
+
     class Meta:
         model = User
         fields = (
@@ -56,3 +68,28 @@ class UserSerializer(serializers.ModelSerializer):
             "bio",
             "role",
         )
+
+    @staticmethod
+    def validate_username(value):
+        return validators.validate_username(value)
+
+
+class SignUpSerializer(serializers.Serializer):
+    """Сериализатор регистрации пользователя"""
+
+    username = serializers.CharField(
+        required=True,
+        max_length=settings.USERNAME_MAX_LENGTH,
+        validators=[
+            validators.validate_username,
+        ],
+    )
+    email = serializers.EmailField(
+        required=True, max_length=settings.EMAIL_MAX_LENGTH
+    )
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        pass
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
 from api.permissions import IsAdminOrReadOnly
-from api.serializers import CategorySerializer, GenreSerializer
+from api.serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    UserSerializer,
+    SignUpSerializer,
+)
+from api.utils import send_confirmation_code, generate_confirmation_code
 from reviews.models import Category, Genre, Title
+
+User = get_user_model()
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -20,3 +32,45 @@ class GenreViewSet(viewsets.ModelViewSet):
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     permission_classes = (IsAdminOrReadOnly,)
+
+
+class GetTokenView(ObtainAuthToken):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    # def post(self, request, *args, **kwargs):
+    #     serializer = UserSerializer(data=request.data, many=False)
+    #     if serializer.is_valid():
+    #         user = serializer.validated_data
+    #         token, created = Token.objects.get_or_create(user=user)
+    #         return Response(
+    #             {
+    #                 "token": token.key,
+    #             }
+    #         )
+    #     else:
+    #         return Response(
+    #             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+    #         )
+
+
+class SignUpView(ObtainAuthToken):
+    """Регистрация пользователя"""
+
+    def post(self, request, *args, **kwargs):
+        serializer = SignUpSerializer(data=request.data, many=False)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.confirmation_code = generate_confirmation_code()
+            user.save()
+            send_confirmation_code(user)
+            return Response(
+                {
+                    "email": user.email,
+                    "username": user.username,
+                }
+            )
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
