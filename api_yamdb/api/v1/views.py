4d2844c53db8ff651f445573e3a6f684f7d6cad7
Lashkin Sergey <lashkinse@yandex.ru>
2023-01-30 21:29:36 +0400

    Добавил bulk_create

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -6,21 +6,28 @@ from rest_framework import status, views, viewsets
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from reviews.models import Category, Comment, Genre, Review, Title
-
 from .filters import TitleFilter
 from .mixins import CustomMixin
 from .permissions import IsAdmin, IsAdminOrReadOnly, IsStaffOrAuthorOrReadonly
-from .serializers import (CategorySerializer, CommentSerializer,
-                          GenreSerializer, GetTokenSerializer,
-                          ReviewSerializer, SignUpSerializer,
-                          TitleReadSerializer, TitleWriteSerializer,
-                          UserSerializer)
+from .serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    GetTokenSerializer,
+    ReviewSerializer,
+    SignUpSerializer,
+    TitleReadSerializer,
+    TitleWriteSerializer,
+    UserSerializer,
+)
 from .utils import generate_confirmation_code, send_confirmation_code
 
 User = get_user_model()
@@ -181,9 +188,17 @@ class CommentViewSet(viewsets.ModelViewSet):
     )
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        review = get_object_or_404(
+            Review,
+            pk=self.kwargs.get("review_id"),
+            title=self.kwargs.get("title_id"),
+        )
         return Comment.objects.filter(review=review)
 
     def perform_create(self, serializer):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        review = get_object_or_404(
+            Review,
+            pk=self.kwargs.get("review_id"),
+            title=self.kwargs.get("title_id"),
+        )
         serializer.save(author=self.request.user, review=review)
--- api_yamdb/reviews/management/commands/importcsv.py
+++ api_yamdb/reviews/management/commands/importcsv.py
@@ -17,101 +17,122 @@ class Command(BaseCommand):
         """Imports the users from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "users.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                User.objects.create(
-                    id=row[0],
-                    username=row[1],
-                    email=row[2],
-                    role=row[3],
-                    bio=row[4],
-                    first_name=row[5],
-                    last_name=row[6],
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    User(
+                        id=row[0],
+                        username=row[1],
+                        email=row[2],
+                        role=row[3],
+                        bio=row[4],
+                        first_name=row[5],
+                        last_name=row[6],
+                    ),
                 )
+        User.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_categories_from_csv():
         """Imports the categories from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "category.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                Category.objects.create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2],
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    Category(
+                        id=row[0],
+                        name=row[1],
+                        slug=row[2],
+                    ),
                 )
+            Category.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_comments_from_csv():
         """Imports the comments from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "comments.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                Comment.objects.create(
-                    id=row[0],
-                    review=Review.objects.get(id=row[1]),
-                    text=row[2],
-                    author=User.objects.get(id=row[3]),
-                    pub_date=row[4],
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    Comment(
+                        id=row[0],
+                        review=Review.objects.get(id=row[1]),
+                        text=row[2],
+                        author=User.objects.get(id=row[3]),
+                        pub_date=row[4],
+                    ),
                 )
+            Comment.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_genres_from_csv():
         """Imports the genres from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "genre.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                Genre.objects.create(
-                    id=row[0],
-                    name=row[1],
-                    slug=row[2],
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    Genre(
+                        id=row[0],
+                        name=row[1],
+                        slug=row[2],
+                    ),
                 )
+            Genre.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_titles_from_csv():
         """Imports the titles from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "titles.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                Title.objects.create(
-                    id=row[0],
-                    name=row[1],
-                    year=row[2],
-                    category=Category.objects.get(id=row[3]),
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    Title(
+                        id=row[0],
+                        name=row[1],
+                        year=row[2],
+                        category=Category.objects.get(id=row[3]),
+                    ),
                 )
+            Title.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_reviews_from_csv():
         """Imports the reviews from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "review.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                Review.objects.create(
-                    id=row[0],
-                    title=Title.objects.get(id=row[1]),
-                    text=row[2],
-                    author=User.objects.get(id=row[3]),
-                    score=row[4],
-                    pub_date=row[5],
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    Review(
+                        id=row[0],
+                        title=Title.objects.get(id=row[1]),
+                        text=row[2],
+                        author=User.objects.get(id=row[3]),
+                        score=row[4],
+                        pub_date=row[5],
+                    ),
                 )
+            Review.objects.bulk_create(obj_list)
 
     @staticmethod
     def import_genre_titles_from_csv():
         """Imports the genre titles from the CSV file into the database"""
         with open(settings.CSV_FILES_DIR / "genre_title.csv", "rt") as f:
             f.readline()
-            reader = csv.reader(f, dialect="excel")
-            for row in reader:
-                GenreTitle.objects.create(
-                    id=row[0],
-                    title=Title.objects.get(id=row[1]),
-                    genre=Genre.objects.get(id=row[2]),
+            obj_list = []
+            for row in csv.reader(f, dialect="excel"):
+                obj_list.append(
+                    GenreTitle(
+                        id=row[0],
+                        title=Title.objects.get(id=row[1]),
+                        genre=Genre.objects.get(id=row[2]),
+                    ),
                 )
+            GenreTitle.objects.bulk_create(obj_list)
 
     def handle(self, *args, **options):
         try:
